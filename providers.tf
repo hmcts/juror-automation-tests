0b589d6b065a59a604dc1dad00360c0c82b52d5b
Neil Perry <neil.perry@cgi.com>
2024-05-23 16:39:03 +0100

    add vars

--- providers.tf
+++ providers.tf
@@ -1,3 +1,15 @@
 provider "azurerm" {
   features {}
 }
+
+variable "env" {
+  description = "The deployment environment (sandbox, aat, prod etc..)"
+}
+
+variable "product" {
+  description = "The name of the application"
+}
+
+variable "common_tags" {
+  type = map(string)
+}
