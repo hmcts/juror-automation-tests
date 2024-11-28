903232fda2dfdb6b09079ea989463b339826538e
Danielle Bach <danielle.bach@hmcts.net>
2024-11-28 14:20:47 +0000

    DB - JS-121

--- src/main/java/cucumber/pageObjects/NavigationShared.java
+++ src/main/java/cucumber/pageObjects/NavigationShared.java
@@ -612,7 +612,8 @@ public class NavigationShared {
             log.info("Checkbox " + location_name + " checked:" + checkbox.isSelected());
 
             if (checkbox.isSelected()) {
-                ((JavascriptExecutor) driver).executeScript("if (arguments[0].checked) arguments[0].click();", checkbox);
+                checkbox.sendKeys(Keys.SPACE);
+//                ((JavascriptExecutor) driver).executeScript("if (arguments[0].checked) arguments[0].click();", checkbox);
             } else {
                 log.info("Return");
                 return null;
@@ -1710,7 +1711,7 @@ public class NavigationShared {
                 ));
         log.info("Saw checkbox in row which contained text =>" + nextToText);
         //wait.toBeClickAble(checkbox);
-        checkbox.click();
+        checkbox.sendKeys(Keys.SPACE);
         log.info("Checked checkbox which was in the same row as =>" + nextToText);
 
     }
