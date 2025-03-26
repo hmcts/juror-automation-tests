24b76319b03972b0c6c7c39042f609a2319156e9
osmansalah1 <163840387+osmansalah1@users.noreply.github.com>
2025-03-26 10:07:36 +0000

    Feature/os timeouts 26 03 (#448)
    * using sign In Email button
    
    * changes to login step for AD changes
    
    * adding condition for conflicting element
    
    * Tests for JM-7139
    
    * update user sql
    
    * formatting / env change
    
    * fixing timeouts attempt
    
    * fixing timeouts attempt
    
    ---------
    
    Co-authored-by: CadeFaulkner <cade.faulkner@hmcts.net>
    Co-authored-by: lukesmithcgi <149057431+lukesmithcgi@users.noreply.github.com>
    Co-authored-by: osmansalah <osman.salah@cgi.com>

--- src/test/java/cucumber/steps/StepDef_navigation.java
+++ src/test/java/cucumber/steps/StepDef_navigation.java
@@ -12,9 +12,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.util.stream.Collectors;
+import static org.junit.Assert.assertTrue;
+import com.google.common.collect.ImmutableMap;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -1041,5 +1047,11 @@ public class StepDef_navigation {
 		dateField.clear();
 		dateField.sendKeys(formattedDate);
 		dateField.sendKeys(Keys.TAB);
+	}
+
+	@When("^I check the screenshot taken to see if \"([^\"]*)\" is on the page$")
+	public void checkScreenshotForText(String text) {
+		log.info("Checking screenshot for text: " + text);
+		NAV.checkScreenshotForText(text);
 	}
 }
