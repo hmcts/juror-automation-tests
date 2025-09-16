606fa42529784de6e3ef1f22a3682065945aecee
Danielle Bach <danielle.bach@hmcts.net>
2025-09-16 19:51:42 +0100

    DB - JS-396 Court Dashboard

--- src/main/java/cucumber/pageObjects/Dashboard.java
+++ src/main/java/cucumber/pageObjects/Dashboard.java
@@ -24,20 +24,6 @@ public class Dashboard {
     @FindBy(xpath = "//div/canvas[@id='last7DaysStatsChart']/../div[@class='total']")
     WebElement totalDueToAttendNextSevenDays;
 
-
-//    @FindBy(xpath = "//*[@class='govuk-heading-l' and contains(text(),'Summoning progress')]")
-//    WebElement summoningProgressBanner;
-//    @FindBy(id = "courtNameOrLocation")
-//    WebElement courtNameOrLocationInput;
-//    @FindBy(id = "poolType")
-//    WebElement crownRadioButton;
-//    @FindBy(id = "poolType-2")
-//    WebElement civilRadioButton;
-//    @FindBy(id = "poolType-3")
-//    WebElement highRadioButton;
-//    @FindBy(xpath = "//*[@class='govuk-button' and contains(text(),'Search')]")
-//    WebElement searchButton;
-
     public String totalExpectedToday() {
         return totalExpectedToday.getText();
     }
@@ -46,31 +32,4 @@ public class Dashboard {
         return totalExpectedLastSevenDays.getText();
     }
 
-
-//    public boolean summoningProgressBannerIsDisplayed(){return summoningProgressBanner.isDisplayed();}
-//
-//    public void enterCourtCodeForSearch(String courtCode) { courtNameOrLocationInput.sendKeys(courtCode);
-//    }
-//
-//    public void clickCrownRadioButton() {crownRadioButton.click();
-//    }
-//
-//    public void clickCivilRadioButton() {civilRadioButton.click();
-//    }
-//
-//    public void clickHighRadioButton() {highRadioButton.click();
-//    }
-//    public void clickSearch(){searchButton.click();}
-//    public String[] getRowOfTableContainingPoolNumber(String poolCreated){
-//        List<WebElement> rowContainingPool = driver.findElements(By.xpath("//*[text()='" +poolCreated+"']/parent::*/parent::*/td"));
-//        int index = 0;
-//        int noOfElements = rowContainingPool.size();
-//        String[] row = new String[noOfElements];
-//
-//        do {
-//           row[index] = rowContainingPool.get(index).getText();
-//           index++;
-//        } while (index < noOfElements);
-//        return row;
-//    }
 }
